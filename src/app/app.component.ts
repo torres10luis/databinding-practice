9c48a8255d84a4f14240f8b7a5815eae808a19dc
torres10luis <Luis.angel_15@yahoo.com>
2018-06-05 16:57:23 -0700

    databinding update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { Person } from './person/person.component';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Person Details';
+
+
 }
---
+++ src/app/person/person.component.ts
@@ -0,0 +1,10 @@
+
+export class Person {
+
+
+  constructor(public firstName: string, public lastName: string, public gender: string, public city: string ) {
+
+
+  }
+
+}
