7b29e5c1a1a9ffa4f65fa8b371b4f6ece7c70974
torres10luis <Luis.angel_15@yahoo.com>
2018-06-06 12:28:24 -0700

    databinding update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Person } from './person/person.component';
+// import { Person } from './person/person.component';
 import { Component } from '@angular/core';
 
 
-@Component({
+@Component ({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>{{heading}}</h1>
+  <p>
+    Name: {{person.gender ? (person.gender === 'm' ? 'Mr.' : 'Ms.') : ' '}}
+    {{person.name.firstName + ' ' + person.name.lastName}}
+  </p>
+  <p> City: {{person.city}} </p>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Person Details';
+  heading = 'Person Details';
+ //  constructor(public firstName: string, public lastName: string, public gender: string, public city: string ) {}
 
+  person: any = {
+    name: {
+      firstName: 'Luis',
+      lastName: 'Saldana'
+    },
+    gender: 'm',
+    city: 'mexico'
 
+  };
+
+/* getInitial() {
+  if() {
+
+  } else() {
+
+  }
+}
+  getAdress() {
+
+  }
+*/
 }
--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,10 +1,13 @@
 
-export class Person {
+// export class Person {
+
+//   public peoples: any[] = [{
+
+//   }];
+
+//   // constructor(public firstName: string, public lastName: string, public gender: string, public city: string ) {
 
 
-  constructor(public firstName: string, public lastName: string, public gender: string, public city: string ) {
+//   // }
 
-
-  }
-
-}
+// }
